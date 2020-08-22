b8f4dc253808437578a19c92fc69bc2e57ff602a
fmented <fmented@gmail.com>
2020-08-22 19:38:05 +0700

    Create serviceworker.js

---
+++ assets/serviceworker.js
@@ -0,0 +1,54 @@
+{
+ "name": "Browser Drums",
+ "short_name": "Simple Drum",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#fff",
+  "theme_color": "#fff0",
+  "description": "A Simple Browser-based Drum App.",
+
+ "icons": [
+  {
+   "src": "assets/icon/addroid-icon-36x36.png",
+   "sizes": "36x36",
+   "type": "image/png",
+   "density": "0.75"
+  },
+  {
+   "src": "assets/icon/addroid-icon-48x48.png",
+   "sizes": "48x48",
+   "type": "image/png",
+   "density": "1.0"
+  },
+  {
+   "src": "assets/icon/addroid-icon-72x72.png",
+   "sizes": "72x72",
+   "type": "image/png",
+   "density": "1.5"
+  },
+  {
+   "src": "assets/icon/addroid-icon-96x96.png",
+   "sizes": "96x96",
+   "type": "image/png",
+   "density": "2.0"
+  },
+  {
+   "src": "assets/icon/addroid-icon-144x144.png",
+   "sizes": "144x144",
+   "type": "image/png",
+   "density": "3.0"
+  },
+  {
+   "src": "assets/icon/addroid-icon-192x192.png",
+   "sizes": "192x192",
+   "type": "image/png",
+   "density": "4.0"
+  }
+ ],
+ "serviceworker": {
+    "src": "assets/serviceworker.js",
+    "scope": "/",
+    "type": "",
+    "update_via_cache": "none"
+  }
+}
